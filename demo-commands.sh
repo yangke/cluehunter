858f4c8e4f2a5045ed7bdd8d289b20477df6d094
yangke <123yangke321@sina.com>
2016-10-08 15:19:18 +0800

    fix the demo-commands.sh and remove confirm info for robot_dbg.exp

--- demo-commands.sh
+++ demo-commands.sh
@@ -9,8 +9,9 @@ python cluehunter.py -ps '*' -vs mode -l 1 -t test/gdb_logs/speex/CVE-2008-1686/
 #python cluehunter.py -ps 'N' -vs modeID -i 165 -t gdb.txt -n gdb-speex-1.1.12_speexdec_modeID
 
 #swfmill
-#python cluehunter.py -ps 'N' -vs length -t /home/yangke/workspace/cluehunter/test/gdb_logs/swfmill-0.3.3/gdb-swfmill-0.3.3_length.txt -n gdb-swfmill-0.3.3_length
+#python cluehunter.py -ps 'N' -vs length -t test/gdb_logs/swfmill-0.3.3/gdb-swfmill-0.3.3__new_unsigned_char[length]_exploit_0_0.txt -n gdb-swfmill-0.3.3_length
 
 #swftools
+#python cluehunter.py -ps '*' -vs 't->data' -t test/gdb_logs/swftools-0.9.2/swfdump/gdb-swfdump_t-data_i_0.txt -n gdb-swftools-0.9.2_swfdump_t-data_i_0
 #python cluehunter.py -ps '*' -vs 't->data' -t gdb.txt -n gdb-swftools-0.9.2_swfdump_t-data_i
 
